f73a010f27a2495ec6145cd9f3a6d2aed743c0ac
Roghelio <145814012+Roghelio@users.noreply.github.com>
2023-10-05 17:23:39 -0600

    actualizaciones rgh1
    se actualiza colores de btns header, se agrega direcciones a los botnes restantes del header, se actualiza pantalla sucursales y productos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faFacebook, faGoogle, faInstagram, faTwitter, faPaypal, } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faGoogle, faInstagram, faTwitter, faPaypal, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faCircleUser, faCreditCard } from '@fortawesome/free-solid-svg-icons';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -41,7 +41,7 @@ import {MatIconModule} from '@angular/material/icon';
 export class SharedModule { 
   constructor(library: FaIconLibrary) {
     library.addIcons(
-      faFacebook, faGoogle, faInstagram, faTwitter, faCircleUser, faCreditCard, faPaypal
+      faFacebook, faGoogle, faInstagram, faTwitter, faCircleUser, faCreditCard, faPaypal, faXTwitter
     ); 
   }
 }
