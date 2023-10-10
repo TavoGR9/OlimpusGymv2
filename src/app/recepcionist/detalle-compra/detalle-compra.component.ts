d0ce33af9a98b0f3823c7febeff18c0dc49dfc05
17Marco00 <17antoniobol@gmail.com>
2023-10-10 14:36:26 -0600

    pantalla detalle compra hacia carrito
    se le agrego el direccionamiento de la pantalla de detalle compra hacia el carrito

--- src/app/recepcionist/detalle-compra/detalle-compra.component.ts
+++ src/app/recepcionist/detalle-compra/detalle-compra.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ConnectionService } from 'src/app/servicios/connection.service';
 
 @Component({
   selector: 'app-detalle-compra',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./detalle-compra.component.css']
 })
 export class DetalleCompraComponent {
+  hide = true;
+  form: FormGroup;
 
+  constructor (private fb: FormBuilder,
+    private router: Router,
+    private http: ConnectionService,
+    private toastr: ToastrService ){
+    this.form = this.fb.group({
+      data: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  navegarPagina(url: String): void {
+    //console.log("Va a navegar", url);
+    this.router.navigate([ url ]);
+  }
 }
+
