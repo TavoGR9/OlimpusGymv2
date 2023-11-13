2457f74c26f7c3d4617c6862110838a3b6a6f8f9
Roghelio <145814012+Roghelio@users.noreply.github.com>
2023-11-13 15:59:35 -0600

    Btn pago con paypal ok

--- src/app/user/confirmacion/confirmacion.component.ts
+++ src/app/user/confirmacion/confirmacion.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import * as printJS from 'print-js';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-confirmacion',
   templateUrl: './confirmacion.component.html',
   styleUrls: ['./confirmacion.component.css']
 })
-export class ConfirmacionComponent {
+export class ConfirmacionComponent implements OnInit {
+  //Varibles con contenido correspondiente al monto e items de la compra
+  //@Input() amount;
+  //@Input() items;
+  constructor( private spinner: NgxSpinnerService ){}
+
+  ngOnInit(): void {
+  }
+
+  //Metodo que imprime el contenido de un elemento especifico de la pantalla html
+  imprimirPantalla() {
+    //this.spinner.show();
+    printJS({
+      //Se agrega el nombre-de-la-clase-o-id-del-elemento-a-imprimir
+      printable: 'ticket_user',
+      type: 'html',
+      targetStyles: ['*'],
+      documentTitle: 'Recibo de compra'
+    });
+  }
 
 }
--- src/app/user/pago-inscripcion/pago-inscripcion.component.ts
+++ src/app/user/pago-inscripcion/pago-inscripcion.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-
+import { PagoInscripcionService} from 'src/app/servicios/pago-inscripcion.service';
+import { Router,  ActivatedRoute } from '@angular/router';
+import { ICreateOrderRequest, IPayPalConfig } from 'ngx-paypal';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -25,10 +27,24 @@ export class PagoInscripcionComponent implements OnInit {
   correo: any;
   // Inicializa responseData como un objeto vacío
   responseData: any = {};
+  // Creacion de objeto de prueba para validaciones de pago
+  responseData2: any = {
+    "titulo": "Membresia basica",
+    "nombreGym": "Gimansio suc.Panzacola",
+    "duracion": "30",
+    "precio": 299,
+    "ID_Cliente": 59,
+    "email": "pepe_pecas_pica_papas@gmail.com",
+    "detalles": "Acceso completo",
+    "fechaInicio": "10\/11\/2023",
+    "fechaFin": "10\/12\/2023"
+  };
+  //Variable para menejar el pago de Paypal
+  public payPalConfig?: IPayPalConfig;
 
   constructor (private fb: FormBuilder, private payInscripcion: PagoInscripcionService, private activeRoute: ActivatedRoute){
 
-  constructor (private fb: FormBuilder){
+    this.correo = this.activeRoute.snapshot.paramMap.get('idEmail');
 
     this.form = this.fb.group({
       tc_nom: ['', Validators.compose([ Validators.required, Validators.pattern(/^[A-Za-zñÑáéíóú ]*[A-Za-z][A-Za-zñÑáéíóú ]*$/)])],
@@ -41,6 +57,11 @@ export class PagoInscripcionComponent implements OnInit {
   }
 
   cambiarSeleccion(radio: string) {
+    if(radio === 'contenedor1'){
+      this.form.reset();
+      this.form.markAsPristine();
+      this.form.markAsUntouched();
+    }
     this.radioSeleccionado = radio;
   }
 
@@ -71,5 +92,84 @@ export class PagoInscripcionComponent implements OnInit {
       this.fecha = this.fecha.slice(0, 5);
     }
   }
+
+  ngOnInit(): void {
+    this.initConfig();
+    this.payInscripcion.consultarDataPago(this.correo).subscribe(respuesta =>{console.log(respuesta)
+    this.responseData=respuesta;
+    //console.log(this.responseData.titulo) 
+    });
+  }
+
+  //Funcion pago con paypal *********** paypal - paypal - paypal - paypal - paypal - paypal - paypal- paypal
+  private initConfig(): void {
+    this.payPalConfig = {
+    currency: 'MXN',
+    clientId: 'AcXxpxeoadCNQcHM-pX-lS6w1A61T5tDNTXmOtaO2b-gtIyEibZmrgqG_EBNwv03t48H1-whDLWMJoPD',
+    createOrderOnClient: (data) => <ICreateOrderRequest> {
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'MXN',
+            value: this.responseData2.precio,
+            breakdown: {
+              item_total: {
+                currency_code: 'MXN',
+                value: this.responseData2.precio
+              }
+            }
+          },
+          items: [
+            {
+              name: this.responseData2.titulo,
+              quantity: '1',
+              category: 'DIGITAL_GOODS',
+              unit_amount: {
+                currency_code: 'MXN',
+                value: this.responseData2.precio,
+              },
+            }
+          ]
+        }
+      ]
+    },
+    advanced: {
+      commit: 'true'
+    },
+    style: {
+      label: 'paypal',
+      layout: 'vertical'
+    },
+    onApprove: (data, actions) => {
+      console.log('onApprove - transaction was approved, but not authorized', data, actions);
+      actions.order.get().then((details: any) => {
+        console.log('onApprove - you can get full order details inside onApprove: ', details);
+      });
+    },
+    // Metodo que retorna los datos correspondientes al pago realizado por paypal
+    onClientAuthorization: (data) => {
+      console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', JSON.stringify(data));
+      //mandar datos a pantalla recibo
+      //this.componentRecibo();
+    },
+    // Metodo que se activa cuando el usuario cierra la ventana de paypal
+    onCancel: (data, actions) => {
+      console.log('OnCancel', data, actions);
+    },
+    // Metodo que captura t pinta los errores
+    onError: err => {
+      console.log('OnError', err);
+    },
+    // Metodo que manda a llamar - abrir la ventana de paypal
+    onClick: (data, actions) => {
+      console.log('onClick', data, actions);
+    },
+  };
+  }
+/*
+  componentRecibo(items, amount): void {
+    
+  }*/
 
 }
